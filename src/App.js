7561118a25f5e03b8f130d280ea3c360bb2198d1
gideonekekeke <71523437+gideonekekeke@users.noreply.github.com>
2021-11-11 04:56:23 +0100

    added the routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import HeaderView from "./Components/HeaderView";
+import HomeScreen from "./Components/HomeScreen";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          learn ci/c
-        </a>
-      </header>
+    <div>
+      <Router>
+        <HeaderView />
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
